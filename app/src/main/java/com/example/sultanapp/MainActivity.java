8b91c76cc12b9fbe23c41353afec5244f4a49d6c
home <isultan0054@gmail.com>
2021-03-17 20:20:06 +0300

    versoin 1

--- app/src/main/java/com/example/sultanapp/MainActivity.java
+++ app/src/main/java/com/example/sultanapp/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void logout (View view){
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),Login.class));
+
         finish();
     }
 }
